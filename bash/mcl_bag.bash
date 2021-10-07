ac0e02f0b54d8c91da88ba43814023f7fc354393
hrjp <r115159@g.kobe-kosen.ac.jp>
2021-10-07 17:55:30 +0900

    param setting

--- bash/mcl_bag.bash
+++ bash/mcl_bag.bash
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-roslaunch kcctcore bag.launch
+roslaunch kcctcore bag.launch &
 
-rosbag play /home/user/rosbag/2021-09-15-17-34-47.bag --clock --topic /velodyne_points /float_sensor_data /initialpose
+sleep 1;rosbag play /home/user/rosbag/2021-10-07-14-15-16.bag --clock --topic /velodyne_points /float_sensor_data /initialpose &
 
-
-sleep 3s;
-roslaunch kcctcore mcl_3dl.launch 
+sleep 1;roslaunch kcctcore mcl_3dl.launch
